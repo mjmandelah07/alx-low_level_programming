d36e11f4753a8047a430ea853d7f8649b5b62453
mjmandelah07 <aramidemojisolagrace@gmail.com>
2022-04-14 12:16:35 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,12 @@
  */
 void print_line(int n)
 {
-	int len;
-
-	if (n > 0)
-	{
-		for (len = 0; len < n; len++)
-			_putchar('_');
-	}
-
-	_putchar('\n');
+int len;
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+_putchar('_');
 }
+_putchar('\n');
+}
+
