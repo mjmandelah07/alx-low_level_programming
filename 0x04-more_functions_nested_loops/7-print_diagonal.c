47acfdb4278fcd12024e88e9927fae9b071c1b71
aramide mojisola oluwadamilola <76177626+mjmandelah07@users.noreply.github.com>
2022-04-15 06:33:48 +0100

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,24 +5,26 @@
  * @n: input value to check
  * Return: nothing.
  */
+
 void print_diagonal(int n)
 {
-int i = 0;
-int j = 0;
-while (i < n)
-{
-while (j <= i)
-{
-if (j != i)
-_putchar(' ');
-else
-_putchar(92);
-j += 1;
-}
-_putchar('\n');
-i += 1;
-j = 0;
-}
-if (n <= 0)
-_putchar('\n');
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		while (j <= i)
+		{
+			if (j != i)
+				_putchar(' ');
+			else
+				_putchar(92);
+			j += 1;
+		}
+		_putchar('\n');
+		i += 1;
+		j = 0;
+	}
+	if (n <= 0)
+		_putchar('\n');
 }
